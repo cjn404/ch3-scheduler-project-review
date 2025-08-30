8969ce1948b08f9e11e5d94b2c3ec59f9c020a1b
cjn404 <moi404@naver.com>
2025-08-31 01:09:52 +0900

    [Lv2] feat: UserRequest 구현

---
+++ src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserRequest.java
@@ -0,0 +1,11 @@
+package org.example.ch3schedulerprojectreview.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRequest {
+
+    private String email;
+    private String password;
+    private String userName;
+}
