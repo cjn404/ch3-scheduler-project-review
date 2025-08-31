2cd1b3346ae09ca92a9cbb34668447324518b66e
cjn404 <moi404@naver.com>
2025-09-01 08:04:05 +0900

    [Lv5] fix: 비밀번호 해시화 오류 수정

--- src/main/java/org/example/ch3schedulerprojectreview/config/PasswordEncoder.java
+++ src/main/java/org/example/ch3schedulerprojectreview/config/PasswordEncoder.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Component;
 public class PasswordEncoder {
 
     public String encode(String rawPassword) {
-        return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
+        return BCrypt.withDefaults().hashToString(10, rawPassword.toCharArray());
     }
 
     public boolean matches(String rawPassword, String encodedPassword) {
-        BCrypt.Result result = BCrypt.verifyer().verify(encodedPassword.toCharArray(), rawPassword);
+        BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
     }
 }
