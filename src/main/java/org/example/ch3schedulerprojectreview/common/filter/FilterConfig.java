bed640b2d952b3147c9060809e19c0ebeb1853dd
cjn404 <moi404@naver.com>
2025-08-31 13:47:15 +0900

    [Lv3] feat: 필터 순서 지정

--- src/main/java/org/example/ch3schedulerprojectreview/common/filter/FilterConfig.java
+++ src/main/java/org/example/ch3schedulerprojectreview/common/filter/FilterConfig.java
@@ -3,6 +3,7 @@ package org.example.ch3schedulerprojectreview.common.filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 @Configuration
 public class FilterConfig {
@@ -12,6 +13,7 @@ public class FilterConfig {
         FilterRegistrationBean<LoginFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginFilter());
         filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
 
         return filterRegistrationBean;
     }
