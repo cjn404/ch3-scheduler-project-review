029b1d129c5a05ec73a704ee5d76d716730e9e9c
cjn404 <moi404@naver.com>
2025-08-31 21:32:54 +0900

    [Lv3] feat: 조건 추가

--- src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserRequest.java
+++ src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserRequest.java
@@ -1,11 +1,25 @@
 package org.example.ch3schedulerprojectreview.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserRequest {
 
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
+
+    // 비밀번호: 8~16자, 대소문자, 숫자, 특수문자 최소 1개씩 포함
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    @Size(min = 2, max = 30, message = "사용자 이름은 2~30자입니다.")
     private String userName;
 }
--- src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserUpdateRequest.java
+++ src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserUpdateRequest.java
@@ -1,10 +1,19 @@
 package org.example.ch3schedulerprojectreview.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequest {
 
+    // 비밀번호: 8~16자, 대소문자, 숫자, 특수문자 최소 1개씩 포함
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    @Size(min = 2, max = 30, message = "사용자 이름은 2~30자입니다.")
     private String userName;
 }
