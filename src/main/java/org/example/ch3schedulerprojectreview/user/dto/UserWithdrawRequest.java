75a1132118e07cd42257cd39ce1d676c505f1a91
cjn404 <moi404@naver.com>
2025-09-01 05:20:46 +0900

    [Lv4] feat: UserWithdrawRequest 생성

---
+++ src/main/java/org/example/ch3schedulerprojectreview/user/dto/UserWithdrawRequest.java
@@ -0,0 +1,14 @@
+package org.example.ch3schedulerprojectreview.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class UserWithdrawRequest {
+
+    // 비밀번호: 8~16자, 대소문자, 숫자, 특수문자 최소 1개씩 포함
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+}
